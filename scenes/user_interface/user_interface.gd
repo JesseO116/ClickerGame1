6c0fcc701db1dc53f2d6bb06ebae87e6a80d364f
Jesse Ortiz <ortizjesse116@gmail.com>
2025-06-25 14:42:59 -0500

    init

--- scenes/user_interface/user_interface.gd
+++ scenes/user_interface/user_interface.gd
@@ -1,17 +1,19 @@
 class_name UserInterface
 extends Control
 
-
+## List of views.
 enum Views {
 	PROTO_CLICKER,
 	PROTO_GENERATOR,
 }
 
+## Signals the various views.
 signal navigation_requested(view : Views)
 
-
+## When Generate Pets link is pressed, signals the generation view.
 func _on_gen_pets_link_pressed() -> void:
 	navigation_requested.emit(Views.PROTO_GENERATOR)
 
-func _on_clicker_link_pressed() -> void:
+## When Home link is pressed, signals the home view. 
+func _on_home_link_pressed() -> void:
 	navigation_requested.emit(Views.PROTO_CLICKER)
